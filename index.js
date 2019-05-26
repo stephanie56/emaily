49d9396009cf807a90df5e40ad4e69df9cbe7463
Stephanie Zeng <stephanie.zeng@rangle.io>
2019-05-26 05:39:34 -0400

    Enable cookie session

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
 require("./models/User");
 require("./services/passport");
 
@@ -8,10 +10,19 @@ const authRoutes = require("./routes/authRoutes");
 
 mongoose
   .connect(process.env.DB_URI, { useNewUrlParser: true })
-  .then(data => console.log("Connected to DB success!"))
+  .then(_ => console.log("Connected to DB success!"))
   .catch(err => console.log(err));
 
 const app = express();
+app.use(
+  cookieSession({
+    // Expires cookies in 30 days
+    maxAge: 30 * 24 * 3600 * 1000,
+    keys: [process.env.COOKIE_KEY]
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 authRoutes(app);
 
